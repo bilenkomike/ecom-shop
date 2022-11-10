20c748db2151ae37b320002624d091e95058b161
Mike Bilenko <80154023+bilenkomike@users.noreply.github.com>
2022-11-10 17:28:38 +0200

    configuring backend data models

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -129,10 +129,20 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+MEDIA_URL = '/images/'
+
+STATIC_FILES_DIRS = [
+    BASE_DIR / 'static'    
+]
+
+MEDIA_ROOT = 'static/images'
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 
 
 
