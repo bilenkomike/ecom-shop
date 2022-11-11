921bba9933c8af778fbf5a4af4e1cc2aa42816ca
Mike Bilenko <80154023+bilenkomike@users.noreply.github.com>
2022-11-11 16:02:14 +0200

    user authentication in backend

--- backend/base/views/product_views.py
+++ backend/base/views/product_views.py
@@ -1,18 +1,10 @@
-from django.shortcuts import render
-from django.http import JsonResponse
-
-
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
+from base.models import Product
+from base.serializers import ProductSerializer
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
-from .models import Product
 
-from .serializers import ProductSerializer
-# Create your views here.
-
-def getRoutes(request):
-
-    return JsonResponse('Hello', safe=False)
 
 @api_view(['GET'])
 def getProducts(request):
@@ -21,10 +13,11 @@ def getProducts(request):
     return Response(serializer.data)
 
 
-
 @api_view(['GET'])
 def getProduct(request, pk):
     product = Product.objects.get(_id=pk)
     serializer = ProductSerializer(product, many=False)
 
     return Response(serializer.data)
+
+
