1ea46715d95935268db1219dcc6d2d9e013fc78d
Mike Bilenko <80154023+bilenkomike@users.noreply.github.com>
2022-11-11 17:40:11 +0200

    user register

--- frontend/src/store.jsx
+++ frontend/src/store.jsx
@@ -4,13 +4,14 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 
 import { productListReducer,productDetailsReducer } from './reducers/productsReducers'
 import { cartReducer } from './reducers/cartReducers';
-import { userLoginReducer } from './reducers/usersReducers';
+import { userLoginReducer, userRegisterReducer } from './reducers/usersReducers';
 
 const reducer = combineReducers({
     productList: productListReducer,
     productDetails:productDetailsReducer,
     cart: cartReducer,
     userLogin: userLoginReducer,
+    userRegister: userRegisterReducer,
 });
 
 
